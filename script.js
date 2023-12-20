90ad1af57b51e8e468a2ba53e1ef8cb032147240
Wipse <wisse.heida@hotmail.com>
2023-12-20 10:59:13 +0100

    Leuke commit hihi

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function addConfirm() {
     document.getElementById('myModal').style.display = 'none';
   }
   
+
+  // vanaf hier voor aanmeld pagina
+
+  let onderlichfilter = getElementById("filter-onderlichaam")
+  let bovenlichfilter = getElementById("filter-bovenlichaam")
+  let allesfilter = getElementById("filter-alles")
+
+  onderlichfilter.addEventListener("click", function() {
+    onderlichfilter.classList.add("selected");
+    bovenlichfilter.classList.remove("selected");
+    allesfilter.classList.remove("selected");
+})
+
+bovenlichfilter.addEventListener("click", function() {
+    bovenlichfilter.classList.add("selected");
+    onderlichfilter.classList.remove("selected");
+    allesfilter.classList.remove("selected");
+})
+
+allesfilter.addEventListener("click", function() {
+    allesfilter.classList.add("selected");
+    onderlichfilter.classList.remove("selected");
+    bovenlichfilter.classList.remove("selected");
+})
